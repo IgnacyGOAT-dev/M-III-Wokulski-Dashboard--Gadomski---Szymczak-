5ca75e5c3316d7a6e91b206a7a84346710652f45
IgnacyGOAT-dev <ignacy.gadomski@cyfroweszkoly.pl>
2026-03-31 09:01:23 +0200

    Create Transaction data class

--- app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
+++ app/src/main/java/com/example/wokolskidashboard/model/Transaction.kt
@@ -1,2 +1,7 @@
 package com.example.wokolskidashboard.model
 
+enum class TransactionType {
+    INCOME, EXPENSE
+}
+
+data class Transaction(val name: String, var price: Int, val type: TransactionType)
