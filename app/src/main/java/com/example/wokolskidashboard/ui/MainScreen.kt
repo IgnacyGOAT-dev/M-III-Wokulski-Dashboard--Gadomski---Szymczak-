6ffb1894bdf95f330cbde8b391a398ed230a8d10
IgnacyGOAT-dev <ignacy.gadomski@cyfroweszkoly.pl>
2026-03-31 09:27:47 +0200

    MainScreen + ExpenseForm

--- app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/MainScreen.kt
@@ -1,2 +1,12 @@
 package com.example.wokolskidashboard.ui
 
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.wokolskidashboard.ui.components.ExpenseForm
+
+@Composable
+fun MainScreen(modifier: Modifier = Modifier){
+    Text("Test")
+    ExpenseForm()
+}
--- app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
+++ app/src/main/java/com/example/wokolskidashboard/ui/components/ExpenseForm.kt
@@ -1,2 +1,46 @@
 package com.example.wokolskidashboard.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ExpenseForm(){
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .padding(12.dp),
+        Arrangement.spacedBy(8.dp),
+        Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Formularz Wydatków",
+            fontSize = 30.sp,
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+        TextField(
+            value = "jakis tekst",
+            onValueChange = {},
+            label = { Text("Enter yoour name:") },
+            singleLine = true
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = {
+                println("Button Clicked")
+            }) {
+            Text("Show name")
+        }
+    }
+}
